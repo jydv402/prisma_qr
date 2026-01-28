0f2e98618fc211d946b8f06ec911551a6a39d652
jydv402 <jydv402@gmail.com>
2026-01-29 01:17:07 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/prisma_qr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prisma_qr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- qr_scan_project/android/app/src/main/kotlin/com/example/qr_catto/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.qr_catto
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
